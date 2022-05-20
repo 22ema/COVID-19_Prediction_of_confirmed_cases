b58d03196b0b899e86a641cb932f6dd9b810632f
yunsung han <yunsung9503@gmail.com>
2022-05-20 21:25:24 +0900

    update train.py code

--- train.py
+++ train.py
@@ -10,6 +10,9 @@ from utils.MakeDataset import MakeDataset
 from tqdm import tqdm
 from model.LSTM import CovidPredictor
 
+def MAE(true, pred):
+    return np.mean(np.abs(true-pred))
+
 def make_Tensor(array):
     return torch.from_numpy(array).float()
 
@@ -27,35 +30,54 @@ def create_squences(data, seq_length):
     return np.array(xs), np.array(ys)
 
 def train_model(model, num_epochs, dataloader_dict_, verbose=10, patience=10):
-    loss_func = nn.L1Loss()
+    criterion = nn.L1Loss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    train_hist = []
-    val_hist = []
+    train_hist = list()
+    val_hist = list()
     for t in range(num_epochs):
-        epoch_loss = 0
+        print("")
+        print('---------------------')
+        print("epoch :{}".format(t+1))
+
         for phase in ['train', 'val']:
+            epoch_loss = 0
             if phase == 'train':
                 model.train()
             else:
                 model.eval()
-            # for idx, seq in tqdm(dataloader_dict_[phase]): ## 여기 수정중
-            #     model.reset_hidden_state() # seq 별 hidden state reset
-            #
-            #     # train_loss
-            #     val_seq = torch.unsqueeze(val_seq, 0)
-            #     y_pred = model(seq)
-            #     loss = loss_func(y_pred[0].float(), train_labels[idx])
-            #
-            #     #update weights
-            #     optimizer.zero_grad()
-            #     loss.backward()
-            #     optimizer.step()
-            #
-            #     epoch_loss += loss.item()
-            # train_hist.append(epoch_loss/len(train_data))
-            #
-            # if val_data is not None:
-            #     with torch.no_grad():
+            for inputs, labels in tqdm(dataloader_dict_[phase]): ## 여기 수정중
+                optimizer.zero_grad()
+                with torch.set_grad_enabled(phase=='train'):
+                    model.reset_hidden_state() # seq 별 hidden state reset
+                    # train_loss
+                    pred = model(inputs)
+                    loss = criterion(pred, labels)
+                    #update weights
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                epoch_loss += loss.item() * inputs.size(0)
+            if phase == 'train':
+                train_hist.append(epoch_loss/len(dataloader_dict_[phase].dataset))
+            else:
+                val_hist.append(epoch_loss/len(dataloader_dict_[phase].dataset))
+        if (t+1) % verbose==0:
+            print("")
+            print(f'Epoch {t+1} train loss: {train_hist[-1]} val loss: {val_hist[-1]}')
+    return model, train_hist, val_hist
+
+def inferencing(model, test_dataset):
+    model = model.eval()
+    with torch.no_grad():
+        preds = list()
+        for inputs, idx in test_dataset:
+            model.reset_hidden_state()
+            y_test_pred = model(inputs)
+            pred = torch.flatten(y_test_pred).item()
+            preds.append(pred)
+    return preds
 
 if __name__ == "__main__":
     # make dataset
@@ -63,7 +85,6 @@ if __name__ == "__main__":
     seq_length = 5
     train_size = int(327 * 0.8)
     batch_size = 10
-    model = CovidPredictor()
 
     time_series_data = pd.read_csv(time_series)
     korea = time_series_data[time_series_data["Country/Region"] == "Korea, South"].iloc[:, 4:].T
@@ -85,10 +106,30 @@ if __name__ == "__main__":
     y_val = MinMaxScaling(y_val, MIN, MAX)
     X_test = MinMaxScaling(X_test, MIN, MAX)
     y_test = MinMaxScaling(y_test, MIN, MAX)
+    X_train = make_Tensor(X_train)
+    y_train = make_Tensor(y_train)
+    X_val = make_Tensor(X_val)
+    y_val = make_Tensor(y_val)
+    X_test = make_Tensor(X_test)
+    y_test = make_Tensor(y_test)
     train_dataset = MakeDataset(X_train, y_train)
     val_dataset = MakeDataset(X_val, y_val)
     test_dataset = MakeDataset(X_test, y_test)
-    train_dataloader = data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    val_dataloader = data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
+    train_dataloader = data.DataLoader(train_dataset, batch_size=1, shuffle=True)
+    val_dataloader = data.DataLoader(val_dataset, batch_size=1, shuffle=True)
+    test_dataloader = data.DataLoader(test_dataset, batch_size=1, shuffle=False)
     dataloader_dict = {'train': train_dataloader, 'val': val_dataloader}
-    train_model(model, 100, dataloader_dict, verbose=10, patience=10)
+    model = CovidPredictor(n_features=1, n_hidden=4, seq_len=seq_length, n_layers=1)
+    model, train_hist, val_hist = train_model(model, 100, dataloader_dict, verbose=10, patience=10)
+    plt.plot(train_hist, label="Training loss")
+    plt.plot(val_hist, label="Val loss")
+    plt.legend()
+    plt.show()
+    preds = inferencing(model, test_dataloader)
+    MAE_result = MAE(np.array(y_test)*MAX, np.array(preds)*MAX)
+    print(MAE_result)
+    plt.plot(daily_cases.index[-len(y_test):], np.array(y_test) * MAX, label='True')
+    plt.plot(daily_cases.index[-len(preds):], np.array(preds) * MAX, label='Pred')
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.show()
--- utils/MakeDataset.py
+++ utils/MakeDataset.py
@@ -7,5 +7,8 @@ class MakeDataset(data.Dataset):
         self.datas = datas
         self.labels = labels
 
+    def __len__(self):
+        return len(self.datas)
+
     def __getitem__(self, index):
         return self.datas[index], self.labels[index]
